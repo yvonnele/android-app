88427e8e2fd6170039032ccc98d1a7ba38f23020
Marcus Nordström <marcus@Marcuss-MacBook-Pro.local>
2013-01-27 23:47:53 +0100

    Added lock button and it's corresponding text

--- gen/com/hyperactivity/android_app/R.java
+++ gen/com/hyperactivity/android_app/R.java
@@ -12,9 +12,14 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int locked=0x7f020001;
+        public static final int unlocked=0x7f020002;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int imageButton1=0x7f070000;
+        public static final int menu_settings=0x7f070003;
+        public static final int textView1=0x7f070001;
+        public static final int textView2=0x7f070002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/hyperactivity/android_app/MainActivity.java
+++ src/com/hyperactivity/android_app/MainActivity.java
@@ -2,14 +2,56 @@ package com.hyperactivity.android_app;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+	
+	private Boolean isLocked;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // Set up our lock button stuff
+        isLocked = true;
+        final TextView lockText = (TextView)findViewById(R.id.textView2);
+        
+        final Drawable lockedImg = getResources().getDrawable(R.drawable.locked);
+        final Drawable unlockedImg = getResources().getDrawable(R.drawable.unlocked);
+        final ImageButton lockButton = (ImageButton)findViewById(R.id.imageButton1);
+		
+        final String lockedText = "Locked";
+        final String unlockedText = "Unlocked";
+        
+        lockText.setText(lockedText);
+        
+        lockButton.setBackgroundDrawable(lockedImg);
+        lockButton.invalidate();
+		
+        lockButton.setOnClickListener(new OnClickListener()
+        {
+			@Override
+			public void onClick(View arg0) {
+				if (isLocked) {
+					lockButton.setBackgroundDrawable(unlockedImg);
+					lockText.setText(unlockedText);
+				}
+				else {
+					lockButton.setBackgroundDrawable(lockedImg);
+					lockText.setText(lockedText);
+				}
+				
+				lockButton.invalidate();
+				isLocked = !isLocked;
+				
+			}});
+       
     }
 
     @Override
@@ -18,5 +60,4 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
-    
 }
